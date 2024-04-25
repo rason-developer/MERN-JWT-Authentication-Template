eea04c71e17b2b8ddf3ae08eae293b57a8ca3664
Rason <rasondeveloper@gmail.com>
2024-04-25 15:17:12 +0100

    +fix(mongoose connection)

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -4,6 +4,8 @@ import cors from 'cors';
 import morgan from 'morgan';
 import http from "http";
 import router from  "./routes";
+import mongoose from "mongoose";
+import { CONNECTION_URL } from './db/connection';
 
 const app = express();
 
@@ -16,4 +18,12 @@ server.listen(4455, () => {
     console.log("Server Running on http://localhost:4455/");
 });
 
+mongoose.Promise = Promise;
+mongoose.connect(CONNECTION_URL);
+mongoose.connection.on("error", (error:Error) => {
+    console.log(error);
+})
+
 app.use('/', router());
+
+
--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -7,7 +7,7 @@ export class Login extends Component {
       <div className="hero-content flex-col lg:flex-row-reverse">
         <div className="text-center lg:text-left">
           <h1 className="text-5xl font-bold">Login now!</h1>
-          <p className="py-6">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+          <p className="py-6">Insert The Ur Email and Password to login!</p>
         </div>
         <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
           <form className="card-body">
